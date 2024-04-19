f26a76450213110ecab8ad6fee3087342bb2a429
chickenfoot <63611976+jbensonj@users.noreply.github.com>
2024-04-19 04:35:42 -0400

    Update BinarySearch.asm

--- BinarySearch.asm
+++ BinarySearch.asm
@@ -1,1 +1,11 @@
 ;Binary Search method goes here
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess proto, dwExitCode:dword
+
+.data
+array DWORD 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+.code
+main PROC
